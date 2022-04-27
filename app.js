38b1cb857271a1c93ac8631afc62cb72d6396c26
Sidharth <sidharth.a.satheesh@gmail.com>
2022-04-27 21:32:18 +0530

    uploaded

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express"),
+  app = express();
+
+//setting view engine to ejs
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+//route for index page
+app.get("/", function (req, res) {
+  res.render("index");
+});
+app.get("/projects", function (req, res) {
+    res.render("projects");
+  });
+//route for magic page
+// app.get("/magic", function (req, res) {
+//   res.render("magic");
+// });
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000 ");
+});
