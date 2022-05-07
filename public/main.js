7f95ab9a80951d585b47a2deb7629d05b9af8dcb
Sidharth <sidharth.a.satheesh@gmail.com>
2022-05-07 14:27:36 +0530

    hover

--- public/main.js
+++ public/main.js
@@ -9,11 +9,16 @@ const el2 = document.getElementById('section2-1');
 const hiddenDiv2 = document.getElementById('hidden-2');
 el2.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv2.style.display = 'block';
-    img2.style.filter = "blur(5px)";
+    // img2.style.filter = "brightness(50%)";
+    // img2.style.filter = "blur(5px)";
+    img2.style["-webkit-filter"] = "blur(5px) brightness(50%)";
+    
 });
 el2.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv2.style.display = 'none';
-    img2.style.filter = "blur(0px)";
+    // img2.style.filter = "blur(0px)";
+    // img2.style.filter = "brightness(100%)";
+    img2.style["-webkit-filter"] = "blur(0px) brightness(100%)";
 });
 
 
@@ -22,11 +27,11 @@ const el3 = document.getElementById('div-img-3');
 const hiddenDiv3 = document.getElementById('hidden-3');
 el3.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv3.style.display = 'block';
-    img3.style.filter = "blur(5px)";
+    img3.style["-webkit-filter"] = "blur(5px) brightness(50%)";
 });
 el3.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv3.style.display = 'none';
-    img3.style.filter = "blur(0px)";
+    img3.style["-webkit-filter"] = "blur(0px) brightness(100%)";
 });
 
 
@@ -35,11 +40,12 @@ const el4 = document.getElementById('div-img-4');
 const hiddenDiv4 = document.getElementById('hidden-4');
 el4.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv4.style.display = 'block';
-    img4.style.filter = "blur(5px)";
+    img4.style["-webkit-filter"] = "blur(5px) brightness(50%)";
+
 });
 el4.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv4.style.display = 'none';
-    img4.style.filter = "blur(0px)";
+    img4.style["-webkit-filter"] = "blur(0px) brightness(100%)";
 });
 
 
@@ -48,11 +54,11 @@ const el5 = document.getElementById('div-img-5');
 const hiddenDiv5 = document.getElementById('hidden-5');
 el5.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv5.style.display = 'block';
-    img5.style.filter = "blur(5px)";
+    img5.style["-webkit-filter"] = "blur(5px) brightness(50%)";
 });
 el5.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv5.style.display = 'none';
-    img5.style.filter = "blur(0px)";
+    img5.style["-webkit-filter"] = "blur(0px) brightness(100%)";
 });
 
 
@@ -61,11 +67,11 @@ const el6 = document.getElementById('div-img-6');
 const hiddenDiv6 = document.getElementById('hidden-6');
 el6.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv6.style.display = 'block';
-    img6.style.filter = "blur(5px)";
+    img6.style["-webkit-filter"] = "blur(5px) brightness(50%)";
 });
 el6.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv6.style.display = 'none';
-    img6.style.filter = "blur(0px)";
+    img6.style["-webkit-filter"] = "blur(0px) brightness(100%)";
 });
 
 
@@ -74,11 +80,11 @@ const el7 = document.getElementById('div-img-7');
 const hiddenDiv7 = document.getElementById('hidden-7');
 el7.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv7.style.display = 'block';
-    img7.style.filter = "blur(5px)";
+    img7.style["-webkit-filter"] = "blur(5px) brightness(50%)";
 });
 el7.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv7.style.display = 'none';
-    img7.style.filter = "blur(0px)";
+    img7.style["-webkit-filter"] = "blur(0px) brightness(100%)";
 });
 
 
@@ -87,11 +93,11 @@ const el8 = document.getElementById('div-img-8');
 const hiddenDiv8 = document.getElementById('hidden-8');
 el8.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv8.style.display = 'block';
-    img8.style.filter = "blur(5px)";
+    img8.style["-webkit-filter"] = "blur(5px) brightness(50%)";
 });
 el8.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv8.style.display = 'none';
-    img8.style.filter = "blur(0px)";
+    img8.style["-webkit-filter"] = "blur(0px) brightness(100%)";
 });
 
 
@@ -100,10 +106,10 @@ const el9 = document.getElementById('section2-4');
 const hiddenDiv9 = document.getElementById('hidden-9');
 el9.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv9.style.display = 'block';
-    img9.style.filter = "blur(5px)";
+    img9.style["-webkit-filter"] = "blur(5px) brightness(50%)";
 });
 el9.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv9.style.display = 'none';
-    img9.style.filter = "blur(0px)";
+    img9.style["-webkit-filter"] = "blur(0px) brightness(100%)";
 });
 
