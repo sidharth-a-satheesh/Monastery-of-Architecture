681b52d2961ea0ed00c5e7b811c699571f025d81
Sidharth <sidharth.a.satheesh@gmail.com>
2022-05-07 14:01:40 +0530

    img hover

--- public/main.js
+++ public/main.js
@@ -3,91 +3,107 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
-const el2 = document.getElementById('img-2');
+
+const img2 = document.getElementById('img-2');
+const el2 = document.getElementById('section2-1');
 const hiddenDiv2 = document.getElementById('hidden-2');
 el2.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv2.style.display = 'block';
-    el2.style.filter = "blur(5px)";
+    img2.style.filter = "blur(5px)";
 });
 el2.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv2.style.display = 'none';
-    el2.style.filter = "blur(0px)";
+    img2.style.filter = "blur(0px)";
 });
 
-const el3 = document.getElementById('img-3');
+
+const img3 = document.getElementById('img-3');
+const el3 = document.getElementById('div-img-3');
 const hiddenDiv3 = document.getElementById('hidden-3');
 el3.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv3.style.display = 'block';
-    el3.style.filter = "blur(5px)";
+    img3.style.filter = "blur(5px)";
 });
 el3.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv3.style.display = 'none';
-    el3.style.filter = "blur(0px)";
+    img3.style.filter = "blur(0px)";
 });
 
-const el4 = document.getElementById('img-4');
+
+const img4 = document.getElementById('img-4');
+const el4 = document.getElementById('div-img-4');
 const hiddenDiv4 = document.getElementById('hidden-4');
 el4.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv4.style.display = 'block';
-    el4.style.filter = "blur(5px)";
+    img4.style.filter = "blur(5px)";
 });
 el4.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv4.style.display = 'none';
-    el4.style.filter = "blur(0px)";
+    img4.style.filter = "blur(0px)";
 });
 
-const el5 = document.getElementById('img-5');
+
+const img5 = document.getElementById('img-5');
+const el5 = document.getElementById('div-img-5');
 const hiddenDiv5 = document.getElementById('hidden-5');
 el5.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv5.style.display = 'block';
-    el5.style.filter = "blur(5px)";
+    img5.style.filter = "blur(5px)";
 });
 el5.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv5.style.display = 'none';
-    el5.style.filter = "blur(0px)";
+    img5.style.filter = "blur(0px)";
 });
 
-const el6 = document.getElementById('img-6');
+
+const img6 = document.getElementById('img-6');
+const el6 = document.getElementById('div-img-6');
 const hiddenDiv6 = document.getElementById('hidden-6');
 el6.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv6.style.display = 'block';
-    el6.style.filter = "blur(5px)";
+    img6.style.filter = "blur(5px)";
 });
 el6.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv6.style.display = 'none';
-    el6.style.filter = "blur(0px)";
+    img6.style.filter = "blur(0px)";
 });
 
-const el7 = document.getElementById('img-7');
+
+const img7 = document.getElementById('img-7');
+const el7 = document.getElementById('div-img-7');
 const hiddenDiv7 = document.getElementById('hidden-7');
 el7.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv7.style.display = 'block';
-    el7.style.filter = "blur(5px)";
+    img7.style.filter = "blur(5px)";
 });
 el7.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv7.style.display = 'none';
-    el7.style.filter = "blur(0px)";
+    img7.style.filter = "blur(0px)";
 });
 
-const el8 = document.getElementById('img-8');
+
+const img8 = document.getElementById('img-8');
+const el8 = document.getElementById('div-img-8');
 const hiddenDiv8 = document.getElementById('hidden-8');
 el8.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv8.style.display = 'block';
-    el8.style.filter = "blur(5px)";
+    img8.style.filter = "blur(5px)";
 });
 el8.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv8.style.display = 'none';
-    el8.style.filter = "blur(0px)";
+    img8.style.filter = "blur(0px)";
 });
 
-const el9 = document.getElementById('img-9');
+
+const img9 = document.getElementById('img-9');
+const el9 = document.getElementById('section2-4');
 const hiddenDiv9 = document.getElementById('hidden-9');
 el9.addEventListener('mouseover', function handleMouseOver() {
     hiddenDiv9.style.display = 'block';
-    el9.style.filter = "blur(5px)";
+    img9.style.filter = "blur(5px)";
 });
 el9.addEventListener('mouseout', function handleMouseOut() {
     hiddenDiv9.style.display = 'none';
-    el9.style.filter = "blur(0px)";
+    img9.style.filter = "blur(0px)";
 });
 
