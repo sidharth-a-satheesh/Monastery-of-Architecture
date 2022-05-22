128710db5669c9828bb9f22ceef3130740aa3975
Arshad Danish <arshaddanishthana@gmail.com>
2022-05-22 19:12:47 +0530

    admin home

--- routes/home.js
+++ routes/home.js
@@ -42,8 +42,32 @@ app.post("/", upload.single("img"), async (req, res) => {
   }
 });
 
-app.get("/edit-home", function (req, res) {
-  res.render("admin/a-index");
+app.get("/edit-home", async (req, res) => {
+  const home = await Home.findOne({ id: 1 });
+  res.render("admin/a-index", { home });
+});
+
+app.post("/edit-home", upload.single("img"), async (req, res) => {
+  let reqBody;
+  if (req.file) {
+    const home = await Home.findOne({ id: 1 });
+    await deleteFile(home.img);
+    const result = await uploadFile(req.file);
+    await unlinkFile(req.file.path);
+    reqBody = { ...req.body, img: result.Location };
+  } else {
+    reqBody = { ...req.body };
+  }
+
+  const doc = await Home.findOneAndUpdate({ id: 1 }, reqBody, {
+    new: true,
+  });
+
+  try {
+    res.render("admin/a-index", { home: doc });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = app;
